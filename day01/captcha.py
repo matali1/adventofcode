9213db9f27698eeb4bf9c65bb23a472a1d7b1c83
Alicja Tadych <atadych@princeton.edu>
2018-03-15 14:35:04 -0400

    Testingscenarios + algorithm

--- day01/captcha.py
+++ day01/captcha.py
@@ -1,2 +1,39 @@
-import click
-print("Ala ma kota")
+
+def reduce(text):
+    prev_letter = text[0]
+    clusters = ""
+    current_cluster=""
+    if text[0] == text[len(text)-1]:
+        current_cluster = text[0]
+
+    for letter in text[1:]:
+        if prev_letter == letter:
+            current_cluster = letter
+
+        else:
+            if current_cluster:
+                clusters += current_cluster
+                current_cluster = ""
+        prev_letter = letter
+
+    if current_cluster:
+        clusters += current_cluster
+
+    print "%s -> %s" % (text,clusters)
+    return clusters
+
+def captcha(text):
+    if not text:
+        return 0
+
+    clusters = reduce(text)
+    value = 0
+    for letter in clusters:
+        value += int(letter)
+    return value
+
+
+
+
+if __name__ == '__main__':
+    print captcha("11122")
--- day01/test.py
+++ day01/test.py
@@ -1,1 +1,48 @@
-#Testing
+import unittest
+
+from captcha import captcha,reduce
+class MyTestCase(unittest.TestCase):
+
+    def test_captcha1(self):
+        result = captcha("1122")
+        self.assertEqual(3,result)
+
+
+    def test_captcha2(self):
+        result = captcha("1111")
+        self.assertEqual(1, result)
+
+
+    def test_captcha3(self):
+        result = captcha("1234")
+        self.assertEqual(0, result)
+
+
+    def test_captcha4(self):
+        result = captcha("91212129")
+        self.assertEqual(9, result)
+
+
+    def test_reduce1(self):
+        result = reduce("1122")
+        self.assertEqual("12", result)
+
+
+    def test_reduce2(self):
+        result = reduce("1111")
+        self.assertEqual("1", result)
+
+
+    def test_reduce3(self):
+        result = reduce("1234")
+        self.assertEqual("", result)
+
+
+    def test_reduce4(self):
+        result = reduce("91212129")
+        self.assertEqual("9", result)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
