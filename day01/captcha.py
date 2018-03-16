75d597d32fd28d523013a90bd1f8c9fd1d1c9936
Alicja Tadych <atadych@princeton.edu>
2018-03-16 10:45:40 -0400

    Script and test for day 01

--- day01/captcha.py
+++ day01/captcha.py
@@ -1,26 +1,59 @@
+import click
+@click.command()
+@click.option('--text', help='Provide text to get captcha')
+@click.option('--file', help='File to read the text from')
+def inverse_captcha(text, file):
+    """Provide captcha either from text or file"""
+    if text:
+        print "Captcha is %d" % captcha(text)
+        print "Halfway Captcha is %d" % captcha_halway(text)
+    if file:
+        with open(file) as myfile:
+            text = myfile.read().replace('\n', '')
+            value = captcha(text)
+            print "********* Captcha is %d" % value
+            halfway = captcha_halway(text)
+            print "********** Halfway Captcha is %d" % halfway
 
 def reduce(text):
-    prev_letter = text[0]
+    '''
+    Reduces text by returning only these letters for which the next letter is the same (circular list)
+    :param text: input text
+    :return: reduced text
+    '''
+    prev_letter = text[len(text)-1]
     clusters = ""
-    current_cluster=""
-    if text[0] == text[len(text)-1]:
-        current_cluster = text[0]
-
-    for letter in text[1:]:
-        if prev_letter == letter:
-            current_cluster = letter
-
-        else:
-            if current_cluster:
-                clusters += current_cluster
-                current_cluster = ""
+    for letter in text:
+        if letter == prev_letter:
+            clusters += letter
         prev_letter = letter
-
-    if current_cluster:
-        clusters += current_cluster
 
     print "%s -> %s" % (text,clusters)
     return clusters
+
+
+def reduce_halfway(text):
+    '''
+    Reduces text by returning only these letters for which the next digit halfway arround the circular list ist the same
+    :param text: input text
+    :return: reduced text
+    '''
+
+    text_length = len(text)
+    if text_length %2 == 1:
+        print "ODD NUMBERS"
+        return ""
+    steps = text_length/2
+
+    clusters = ""
+    for idx,letter in enumerate(text):
+        halfway_letter = text[(idx+steps)%text_length]
+        if letter == halfway_letter :
+            clusters += letter
+
+    print "%s -> %s" % (text,clusters)
+    return clusters
+
 
 def captcha(text):
     if not text:
@@ -29,11 +62,28 @@ def captcha(text):
     clusters = reduce(text)
     value = 0
     for letter in clusters:
-        value += int(letter)
+        try:
+            value += int(letter)
+        except ValueError:
+            print "\tIgnoring ", letter
+            continue
     return value
 
 
+def captcha_halway(text):
+    if not text:
+        return 0
 
+    clusters = reduce_halfway(text)
+    value = 0
+    for letter in clusters:
+        try:
+            value += int(letter)
+        except ValueError:
+            print "\tIgnoring ", letter
+            continue
+    return value
 
 if __name__ == '__main__':
+    inverse_captcha()
     print captcha("11122")
--- day01/test.py
+++ day01/test.py
@@ -1,48 +1,104 @@
 import unittest
 
-from captcha import captcha,reduce
-class MyTestCase(unittest.TestCase):
+from captcha import captcha, reduce, captcha_halway, reduce_halfway
 
+class MyTestCase(unittest.TestCase):
     def test_captcha1(self):
         result = captcha("1122")
-        self.assertEqual(3,result)
-
+        self.assertEqual(3, result)
 
     def test_captcha2(self):
         result = captcha("1111")
-        self.assertEqual(1, result)
-
+        self.assertEqual(4, result)
 
     def test_captcha3(self):
         result = captcha("1234")
         self.assertEqual(0, result)
 
+    def test_captcha5(self):
+        result = captcha("1234xx")
+        self.assertEqual(0, result)
 
     def test_captcha4(self):
         result = captcha("91212129")
         self.assertEqual(9, result)
 
+    def test_captcha6(self):
+        result = captcha("11221")
+        self.assertEqual(4, result)
+
+    def test_captcha7(self):
+        result = captcha("111")
+        self.assertEqual(3, result)
 
     def test_reduce1(self):
         result = reduce("1122")
         self.assertEqual("12", result)
 
-
     def test_reduce2(self):
         result = reduce("1111")
-        self.assertEqual("1", result)
-
+        self.assertEqual("1111", result)
 
     def test_reduce3(self):
         result = reduce("1234")
         self.assertEqual("", result)
 
+    def test_reduce5(self):
+        result = reduce("1234xx")
+        self.assertEqual("x", result)
 
     def test_reduce4(self):
         result = reduce("91212129")
         self.assertEqual("9", result)
 
+    def test_reduce5(self):
+        result = reduce("11221")
+        self.assertEqual("112", result)
+
+    def test_reduce4(self):
+        result = reduce("111")
+        self.assertEqual("111", result)
+
+    def test_hf_reduce1(self):
+        result = reduce_halfway("1212")
+        self.assertEqual("1212", result)
+
+    def test_hf_reduce2(self):
+        result = reduce_halfway("1221")
+        self.assertEqual("", result)
+
+    def test_hf_reduce3(self):
+        result = reduce_halfway("123425")
+        self.assertEqual("22", result)
+
+    def test_hf_reduce4(self):
+        result = reduce_halfway("123123")
+        self.assertEqual("123123", result)
+
+    def test_hf_reduce5(self):
+        result = reduce_halfway("12131415")
+        self.assertEqual("1111", result)
+
+    def test_hf_captcha1(self):
+        result = captcha_halway("1212")
+        self.assertEqual(6, result)
+
+    def test_hf_captcha2(self):
+        result = captcha_halway("1221")
+        self.assertEqual(0, result)
+
+    def test_hf_captcha3(self):
+        result = captcha_halway("123425")
+        self.assertEqual(4, result)
+
+    def test_hf_captcha4(self):
+        result = captcha_halway("123123")
+        self.assertEqual(12, result)
+
+    def test_hf_captcha5(self):
+        result = captcha_halway("12131415")
+        self.assertEqual(4, result)
+
 
 if __name__ == '__main__':
     unittest.main()
-
